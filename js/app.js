ada4c94cd0d0e8d09311849c418b6c84b4b7bd1c
jkitchen002 <joseff.kitchen@gmail.com>
2020-10-04 12:32:44 +0100

    javaScript scroll added

--- js/app.js
+++ js/app.js
@@ -8,3 +8,56 @@ function show() {
 aboutBtn.addEventListener('click', () => {
   show();
 });
+
+// ----------Scroll---------------------
+const aboutMe = document.querySelector('#about-me');
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-in');
+
+const portfolioOptions = {
+  threshold: 0,
+  rootMargin: '0px 0px -20px 0px',
+};
+
+const options = {
+  threshold: 0.5,
+  rootMargin: '0px 0px -100px 0px',
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+options);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
+const portfolioOnScroll = new IntersectionObserver(function (
+  entries,
+  portfolioOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      portfolioOnScroll.unobserve(entry.target);
+    }
+  });
+},
+portfolioOptions);
+
+sliders.forEach((slider) => {
+  portfolioOnScroll.observe(slider);
+});
